59cab050b99d8b869b7f765ed61855904217da5b
akshaypatil581 <akshaypatil581@gmail.com>
2020-05-20 23:07:13 -0700

    Added play and pause song and video functionality

---
+++ app.js
@@ -0,0 +1,42 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  // sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+  //time display
+  const timeDisplay = document.querySelector(".time-display");
+  //Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  //Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDash = outlineLength;
+
+  //play sound
+  play.addEventListener("click", () => {
+    checkPlaying(song);
+    song.play();
+  });
+
+  // Create a function specific to stop and play the sound
+  const checkPlaying = song => {
+    if(song.paused) {
+      song.play();
+      video.play();
+      play.src = './svg/pause.svg';
+    } else {
+      song.pause();
+      video.play();
+      play.src = './svg/play.svg';
+    }
+  }
+
+
+
+};
+
+app();
